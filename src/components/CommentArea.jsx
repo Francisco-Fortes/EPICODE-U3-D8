4b0b4a00e91a1b5e1023d8c8f12a0bb7d9ff92d7
Francisco-Fortes <fortesfco@gmail.com>
2022-12-22 22:14:16 +0100

    Update CommentArea.jsx

--- src/components/CommentArea.jsx
+++ src/components/CommentArea.jsx
@@ -14,13 +14,7 @@ const CommentArea = (props) => {
       setIsLoading(true);
       try {
         let response = await fetch(
-          "https://striveschool-api.herokuapp.com/api/comments/" + props.asin,
-          {
-            headers: {
-              Authorization:
-                "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MGI3OWY5NTgxNmI1YjAwMTU5NDA3NDAiLCJpYXQiOjE2MjI2NDY2NzcsImV4cCI6MTYyMzg1NjI3N30.y-rBwB5WAQOWBvWrLlAgTQUrbGulxd2M6cWH3VLyGLw",
-            },
-          }
+          "https://api.openweathermap.org/data/2.5/weather?q=london&appid=278f10ca700a0126316d3fcef6dcd77b"
         );
         console.log(response);
         if (response.ok) {
